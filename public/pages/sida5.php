7598cc4cb6b2ec89df32bf8475ae8e08e115b87a
hihassan1998 <hassanihussain1998@gmail.com>
2025-01-28 20:05:29 +0100

    added page 5

--- public/pages/sida5.php
+++ public/pages/sida5.php
@@ -10,20 +10,7 @@
 
     <h1>Välkommen till Sida 5</h1>
     <p>Denna sida demonstrerar användningen av funktioner i PHP.</p>
-    <?php
-    function calculateSum($array) {
-        return array_sum($array);
-    }
 
-    function calculateAverage($array) {
-        return count($array) > 0 ? array_sum($array) / count($array) : 0;
-    }
-
-    $numbers = [5, 10, 15, 20, 25];
-    echo "<p>Lista av tal: " . implode(", ", $numbers) . "</p>";
-    echo "<p>Summan av talen: " . calculateSum($numbers) . "</p>";
-    echo "<p>Medelvärdet av talen: " . calculateAverage($numbers) . "</p>";
-    ?>
 <?php include '../../includes/footer.php' ?>
 
 </body>
